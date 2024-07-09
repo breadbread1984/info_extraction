a7232b8fd31a623d4ce781b1d1105904271dd8a8
breadbread1984 <breadbread1984@163.com>
2024-07-09 14:57:42 +0800

    update

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from tqdm import tqdm
 import json
 from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
 from models import Llama2, Llama3, CodeLlama
-from summarize import summarize
-from rag import RAG
+from qa import QA
 
 FLAGS = flags.FLAGS
 
@@ -17,8 +16,7 @@ def add_options():
   flags.DEFINE_boolean('locally', default = False, help = 'whether run LLM locally')
   flags.DEFINE_string('output_json', default = 'output.json', help = 'path to output json')
   flags.DEFINE_enum('model', default = 'llama3', enum_values = {'llama2', 'llama3', 'codellama'}, help = 'model name')
-  flags.DEFINE_boolean('recursively', default = True, help = 'summary multiple chunks into one summary')
-  flags.DEFINE_string('instruction', default = 'Focus on details of examples, chemical formula of electrolyte and numerical data of conductivity', help = 'extra instruction')
+  flags.DEFINE_enum('type', default = 'stuff', enum_values = {'stuff', 'map_reduce', 'refine', 'map_rerank'}, help = 'QA chain type')
 
 def main(unused_argv):
   content = list()
@@ -42,15 +40,11 @@ def main(unused_argv):
       else:
         raise Exception('unknown format!')
       text = ''.join([doc.page_content for doc in loader.load()])
-      print('1) summarize %s' % f)
-      summary = summarize(text, detail = 0.5, llm = llm, tokenizer = tokenizer, summarize_recursively = FLAGS.recursively, additional_instructions = FLAGS.instruction)
-
-      print('2) RAG with the summarization')
-      rag_long = RAG(tokenizer, llm, text, locally = FLAGS.locally, db_dir = "long")
-      rag_short = RAG(tokenizer, llm, summary, locally = FLAGS.locally, db_dir = "short")
-      formula, _ = rag_short.query("what is the chemical formula of the electrolyte produced in the example?")
-      materials, _ = rag_short.query("what are the materials used in the example?")
-      conductivity, _ = rag_long.query("what is the conductivity of the electrolyte?")
+      
+      qa = QA(FLAGS.type, tokenizer, llm, text, locally = FLAGS.locally)
+      formula, _ = qa.query("what is the chemical formula of the electrolyte produced in the example?")
+      materials, _ = qa.query("what are the materials used in the example?")
+      conductivity, _ = qa.query("what is the conductivity of the electrolyte?")
       content.append({"patent":f, "summary": summary, "chemical formula": formula, "starting materials": materials, "conductivity": conductivity})
   with open(FLAGS.output_json, 'w', encoding = 'utf-8') as f:
     f.write(json.dumps(content, indent = 2, ensure_ascii = False))
--- prompts.py
+++ prompts.py
@@ -2,21 +2,132 @@
 
 from langchain_core.prompts.prompt import PromptTemplate
 
-def summarize_template(tokenizer, additional_instructions = None, accumulated = False):
+def map_rerank_prompt(tokenizer):
   messages = [
-    {'role': 'system', 'content': 'Rewrite this text in summarized form.' + ("" if additional_instructions is None else f"\n\n{additional_instructions}")},
-    {'role': 'user', 'content': "{chunk}" if accumulated == False else "Previous summaries:\n\n{accumulated_summaries_string}\n\n{chunk}"}
-  ]
-  prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generating_prompt = True)
-  template = PromptTemplate(template = prompt, input_variables = ['chunk'] if accumulated == False else ['chunk', 'accumulated_summaries_string'])
-  return template
+    {"role": "user", "content": """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
-def rag_template(tokenizer):
-  messages = [
-    {'role': 'system', 'content': 'Use the given context to answer the question. If the context doesn\'t give you a clue to the answer, just say you don\'t know and don\'t try to make up an answer. Try to keep your answer brief and to the point.'},
-    {'role': 'user', 'content': 'context: {context}\nquestion:{question}\nanswer:'}
+In addition to giving an answer, also return a score of how fully it answered the user's question. This should be in the following format:
+
+Question: [question here]
+Helpful Answer: [answer here]
+Score: [score between 0 and 100]
+
+How to determine the score:
+- Higher is a better answer
+- Better responds fully to the asked question, with sufficient level of detail
+- If you do not know the answer based on the context, that should be a score of 0
+- Don't be overconfident!
+
+Example #1
+
+Context:
+---------
+Apples are red
+---------
+Question: what color are apples?
+Helpful Answer: red
+Score: 100
+
+Example #2
+
+Context:
+---------
+it was night and the witness forgot his glasses. he was not sure if it was a sports car or an suv
+---------
+Question: what type was the car?
+Helpful Answer: a sports car or an suv
+Score: 60
+
+Example #3
+
+Context:
+---------
+Pears are either red or orange
+---------
+Question: what color are apples?
+Helpful Answer: This document does not answer the question
+Score: 0
+
+Begin!
+
+Context:
+---------
+{context}
+---------
+Question: {question}
+Helpful Answer:"""}
   ]
   prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generating_prompt = True)
   template = PromptTemplate(template = prompt, input_variables = ['context', 'question'])
   return template
 
+def stuff_prompt(tokenizer):
+  messages = [
+    {"role": "system", "content": """Use the following pieces of context to answer the user's question. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+----------------
+{context}"""},
+    {"role": "user", "content": "{question}"}
+  ]
+  prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generating_prompt = True)
+  template = PromptTemplate(template = prompt, input_variables = ["context", "question"])
+  return template
+
+def map_reduce_question_prompt(tokenizer):
+  messages = [
+    {"role": "system", "content": """Use the following portion of a long document to see if any of the text is relevant to answer the question. 
+Return any relevant text verbatim.
+______________________
+{context}"""},
+    {"role": "user", "content": "{question}"}
+  ]
+  prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generating_prompt = True)
+  template = PromptTemplate(template = prompt, input_variables = ['context', 'question'])
+  return template
+
+def map_reduce_combine_prompt(tokenizer):
+  messages = [
+    {'role': "system", "content": """Given the following extracted parts of a long document and a question, create a final answer. 
+If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+______________________
+{summaries}"""},
+    {'role': "user", "content": "{question}"}
+  ]
+  prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generating_prompt = True)
+  template = PromptTemplate(template = prompt, input_variables = ['summaries', 'question'])
+  return template
+
+def refine_question_template(tokenizer):
+  messages = [
+    {"role": "system", "content": (
+    "Context information is below.\n"
+    "------------\n"
+    "{context_str}\n"
+    "------------\n"
+    "Given the context information and not prior knowledge, "
+    "answer any questions"
+)},
+    {"role": "user", "content": "{question}"}
+  ]
+  prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generating_prompt = True)
+  template = PromptTemplate(template = prompt, input_variables = ['context_str', 'question'])
+  return template
+
+def refine_template(tokenizer):
+  messages = [
+    {"role": "user", "content": "{question}"},
+    {"role": "system", "content": "{existing_answer}"},
+    {"role": "user", "content": (
+    "We have the opportunity to refine the existing answer "
+    "(only if needed) with some more context below.\n"
+    "------------\n"
+    "{context_str}\n"
+    "------------\n"
+    "Given the new context, refine the original answer to better "
+    "answer the question. "
+    "If the context isn't useful, return the original answer."
+)}
+  ]
+  prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generation_prompt = True)
+  template = PromptTemplate(template = prompt, input_variables = ['question', 'existing_answer', 'context_str'])
+  return template

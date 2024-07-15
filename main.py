10b92b069435ee4aeb729a17b40c613b4bd835cb
breadbread1984 <breadbread1984@163.com>
2024-07-15 13:58:58 +0800

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
 from models import Llama2, Llama3, CodeLlama, Qwen2
 from qa import QA
-from prompts import extract_example_template, extract_electrolyte_template, extract_precursor_template
+from prompts import extract_example_template, extract_electrolyte_template, extract_precursor_template, extract_conductivity_template
 
 FLAGS = flags.FLAGS
 
@@ -34,9 +34,11 @@ def main(unused_argv):
   example_template = extract_example_template(tokenizer)
   electrolyte_template, electrolyte_parser = extract_electrolyte_template(tokenizer)
   precursor_template, precursor_parser = extract_precursor_template(tokenizer)
+  conductivity_template, conductivity_parser = extract_conductivity_template(tokenizer)
   example_chain = example_template | llm
   electrolyte_chain = electrolyte_template | llm | electrolyte_parser
   precursor_chain = precursor_template | llm | precursor_parser
+  conductivity_chain = conductivity_template | llm | conductivity_parser
 
   for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
     for f in files:
@@ -59,6 +61,9 @@ def main(unused_argv):
       precursors = precursor_chain.invoke({'context': example})
       with open('%s_precursor.txt' % f, 'w') as fp:
         fp.write(json.dumps(precursors))
+      conductivity = conductivity_chain.invoke({'patent': text})
+      with open('%s_conductivity.txt' % f, 'w') as fp:
+        fp.write(json.dumps(conductivity))
 
 if __name__ == "__main__":
   add_options()

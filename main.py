4bf8d65eb873df116d88f7a797c974ed72bd77c1
breadbread1984 <breadbread1984@163.com>
2024-07-15 16:09:19 +0800

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
 from models import Llama2, Llama3, CodeLlama, Qwen2
 from qa import QA
-from prompts import extract_example_template, extract_electrolyte_template, extract_precursor_template, extract_conductivity_template
+from chains import example_chain, electrolyte_chain, precursor_chain, conductivity_chain, synthesis_chain, structure_chain
 
 FLAGS = flags.FLAGS
 
@@ -31,14 +31,12 @@ def main(unused_argv):
   else:
     raise Exception('unknown model!')
   
-  example_template = extract_example_template(tokenizer)
-  electrolyte_template, electrolyte_parser = extract_electrolyte_template(tokenizer)
-  precursor_template, precursor_parser = extract_precursor_template(tokenizer)
-  conductivity_template, conductivity_parser = extract_conductivity_template(tokenizer)
-  example_chain = example_template | llm
-  electrolyte_chain = electrolyte_template | llm | electrolyte_parser
-  precursor_chain = precursor_template | llm | precursor_parser
-  conductivity_chain = conductivity_template | llm | conductivity_parser
+  example_chain_ = example_chain(llm, tokenizer)
+  electrolyte_chain_ = electrolyte_chain(llm, tokenizer)
+  precursor_chain_ = precursor_chain(llm, tokenizer)
+  conductivity_chain_ = conductivity_chain(llm, tokenizer)
+  synthesis_chain_ = synthesis_chain(llm, tokenizer)
+  structure_chain_ = structure_chain(llm, tokenizer)
 
   for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
     for f in files:
@@ -52,18 +50,19 @@ def main(unused_argv):
       else:
         raise Exception('unknown format!')
       text = ''.join([doc.page_content for doc in loader.load()])
-      example = example_chain.invoke({'patent': text})
-      with open('%s_example.txt' % f, 'w') as fp:
-        fp.write(example)
-      electrolyte = electrolyte_chain.invoke({'patent': text})
-      with open('%s_electrolyte.txt' % f, 'w') as fp:
-        fp.write(json.dumps(electrolyte))
-      precursors = precursor_chain.invoke({'context': example})
-      with open('%s_precursor.txt' % f, 'w') as fp:
-        fp.write(json.dumps(precursors))
-      conductivity = conductivity_chain.invoke({'patent': text})
-      with open('%s_conductivity.txt' % f, 'w') as fp:
-        fp.write(json.dumps(conductivity))
+      output = dict()
+      example = example_chain_.invoke({'patent': text})
+      electrolyte = electrolyte_chain_.invoke({'patent': text})
+      output.update(electrolyte)
+      precursors = precursor_chain_.invoke({'context': example})
+      output.update(precursors)
+      conductivity = conductivity_chain_.invoke({'patent': text})
+      output.update(conductivity)
+      synthesis = synthesis_chain_.invoke({'context': example})
+      output.update(synthesis)
+      output['example'] = example
+      with open('%s_meta.txt' % splitext(f)[0], 'w') as fp:
+        fp.write(json.dumps(output, indent = 2))
 
 if __name__ == "__main__":
   add_options()

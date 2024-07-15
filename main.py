3bc5f9bb59f892ba634e95d368b9b93462cc6cd6
breadbread1984 <breadbread1984@163.com>
2024-07-15 10:47:09 +0800

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
 from models import Llama2, Llama3, CodeLlama, Qwen2
 from qa import QA
-from prompts import extract_example_template, extract_precursor_template, customized_template
+from prompts import extract_example_template, extract_electrolyte_template, extract_precursor_template
 
 FLAGS = flags.FLAGS
 
@@ -32,8 +32,10 @@ def main(unused_argv):
     raise Exception('unknown model!')
   
   example_template = extract_example_template(tokenizer)
+  electrolyte_template, electrolyte_parser = extract_electrolyte_template(tokenizer)
   precursor_template, precursor_parser = extract_precursor_template(tokenizer)
   example_chain = example_template | llm
+  electrolyte_chain = electrolyte_template | llm | electrolyte_parser
   precursor_chain = precursor_template | llm | precursor_parser
 
   for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
@@ -51,6 +53,9 @@ def main(unused_argv):
       example = example_chain.invoke({'patent': text})
       with open('%s_example.txt' % f, 'w') as fp:
         fp.write(example)
+      electrolyte = electrolyte_chain.invoke({'patent': text})
+      with open('%s_electrolyte.txt' % f, 'w') as fp:
+        fp.write(json.dumps(electrolyte))
       precursors = precursor_chain.invoke({'context': example})
       with open('%s_precursor.txt' % f, 'w') as fp:
         fp.write(json.dumps(precursors))

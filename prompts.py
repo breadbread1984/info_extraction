98226764f9c3e96f78e4e55a251717ffcc1e0c4a
breadbread1984 <breadbread1984@163.com>
2024-07-26 09:34:41 +0800

    update

--- prompts.py
+++ prompts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from typing import List, Dict
+from typing import List, Dict, Union
 from langchain_core.pydantic_v1 import BaseModel, Field, create_model
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts.prompt import PromptTemplate
@@ -17,10 +17,40 @@ def extract_example_template(tokenizer):
 
 def exists_electrolyte_template(tokenizer):
   class Result(BaseModel):
-    present: str = Field(description = "'present', 'not present' or a spepcific place (table or figure) where the ratio is given")
+    present: str = Field(description = "a string between 'present' and 'not present' will represents whether the ratio of elements is given in the context.")
+    detail: Union[Dict[str,str], str] = Field(description = "if the ratio of elements is present in the context, return a dictionary whose keys are elements and values are their proportion. if the ratio of elements is present outside the context, return where the ratio is given in string format.")
   parser = JsonOutputParser(pydantic_object = Result)
   instructions = parser.get_format_instructions()
-  system_message = """The following text is about how an electrolyte is produced. Please judge whether the ratio of elements in the electrolyte mentioned in the context is given. Please return 'present' if the ratio is present, 'not present' if the ratio is not present or the specific place (table or figure) where the ratio is given.
+  system_message = """The following text is about how an electrolyte is produced. Please extract the elements and their proportions in the electrolyte introduced in the context if the information is available. The ratio of elements can be given by the reading of ICP of the electrolyte or by the chemical formula of the electrolyte. Please extract where the ratio of the elements is given outside the context if the information is not available in this context. The context should refer the reader to a specific table for the ratio of the elements.
+
+Here are two examples of context which gives information about the ratio of elements.
+
+Example 1
+Input context:
+---------------------
+The resultant powdery sulfide-based solid electrolyte was analyzed through powdery X-ray diffraction (XRD) using an X-ray diffractometer (XRD) (Smart Lab Apparatus, manufactured by Rigaku Corporation). Any other peak than the peaks derived from the raw materials was not detected. Analyzed using an ICP emission spectrometric apparatus, the composition was Li:S:P:Br:I (by mol)=1.390:1.590:0.400:0.109:0.101.
+---------------------
+Output electrolyte:
+{"Li":1.390,"S":1.590,"P":0.400,"Br":0.109,"I":0.101}
+
+Example 2:
+Input context:
+---------------------
+(S3) The amorphized powder mixture was crystallized through thermal treatment at a temperature of about 500° C. for 4 hr, thereby yielding a solid electrolyte having an argyrodite-type crystal structure, as represented by Chemical Formula 2 below.
+Li6PS5Cl  [Chemical Formula 2] 
+---------------------
+Output electrolyte:
+{"Li":6.00,"P":1.00,"S":5.00,"Cl":1.00}
+
+Here are an example of context which refers reader to a table
+
+Example 1
+Input context:
+---------------------
+The sulfide solid electrolyte was subjected to an ICP analysis, and the molar ratio of each element was measured. The ionic conductivity and the residual ratio were measured. The results are shown in Table 1.
+---------------------
+Output table:
+"Table 1"
 """ + instructions
   system_message = system_message.replace('{','{{')
   system_message = system_message.replace('}','}}')

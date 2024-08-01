c809e8aadda5e2ce77a1543d0a2796d2efb1798f
breadbread1984 <breadbread1984@163.com>
2024-08-01 15:58:45 +0800

    update

--- chains.py
+++ chains.py
@@ -13,18 +13,18 @@ def example_chain(llm, tokenizer):
   return example_chain
 
 def electrolyte_chain(llm, tokenizer):
-  electrolyte_template, electrolyte_parser = extract_electrolyte_template(tokenizer)
-  electrolyte_chain = electrolyte_template | llm | electrolyte_parser
+  electrolyte_template = extract_electrolyte_template(tokenizer)
+  electrolyte_chain = electrolyte_template | llm
   return electrolyte_chain
 
 def precursor_chain(llm, tokenizer):
-  precursor_template, precursor_parser = extract_precursor_template(tokenizer)
-  precursor_chain = precursor_template | llm | precursor_parser
+  precursor_template = extract_precursor_template(tokenizer)
+  precursor_chain = precursor_template | llm
   return precursor_chain
 
 def conductivity_chain(llm, tokenizer):
-  conductivity_template, conductivity_parser = extract_conductivity_template(tokenizer)
-  conductivity_chain = conductivity_template | llm | conductivity_parser
+  conductivity_template = extract_conductivity_template(tokenizer)
+  conductivity_chain = conductivity_template | llm
   return conductivity_chain
 
 def synthesis_chain(llm, tokenizer):

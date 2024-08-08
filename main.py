4818ec5dcfa73a095c3691af5f9ddf36818a80a0
breadbread1984 <breadbread1984@163.com>
2024-08-08 07:37:45 +0000

    update

--- main.py
+++ main.py
@@ -7,44 +7,38 @@ from absl import flags, app
 from tqdm import tqdm
 import json
 from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
-from models import Llama2, Llama3, CodeLlama, Qwen2, Customized
-from chains import example_chain, electrolyte_chain, precursor_chain, conductivity_chain, synthesis_chain, structure_chain
+from langchain.graphs import Neo4jGraph
+from prompts import extract_triplets_template, cypher_generation_template, entity_generation_template, triplets_qa_template
+from models import Llama3, Qwen2
 
 FLAGS = flags.FLAGS
 
 def add_options():
   flags.DEFINE_string('input_dir', default = None, help = 'path to directory containing pdfs')
   flags.DEFINE_boolean('locally', default = False, help = 'whether run LLM locally')
-  flags.DEFINE_string('output_dir', default = 'output', help = 'path to output directory')
-  flags.DEFINE_enum('model', default = 'customized', enum_values = {'llama2', 'llama3', 'codellama', 'qwen2', 'customized'}, help = 'model name')
-  flags.DEFINE_string('ckpt', default = None, help = 'path to checkpoint')
-  flags.DEFINE_enum('mode', default = 'electrolyte', enum_values = {'electrolyte', 'precursors', 'conductivity'}, help = 'mode')
+  flags.DEFINE_string('host', default = 'bolt://localhost:7687', help = 'host')
+  flags.DEFINE_string('user', default = 'neo4j', help = 'user name')
+  flags.DEFINE_string('password', default = 'neo4j', help = 'password')
+  flags.DEFINE_string('database', default = 'neo4j', help = 'database')
+  flags.DEFINE_enum('model', default = 'qwen2', enum_values = {'llama3', 'qwen2'}, help = 'model name')
 
 def main(unused_argv):
   if exists(FLAGS.output_dir): rmtree(FLAGS.output_dir)
   mkdir(FLAGS.output_dir)
-  if FLAGS.model == 'llama2':
-    tokenizer, llm = Llama2(FLAGS.locally)
-  elif FLAGS.model == 'llama3':
+  if FLAGS.model == 'llama3':
     tokenizer, llm = Llama3(FLAGS.locally)
-  elif FLAGS.model == 'codellama':
-    tokenizer, llm = CodeLlama(FLAGS.locally)
   elif FLAGS.model == 'qwen2':
     tokenizer, llm = Qwen2(FLAGS.locally)
-  elif FLAGS.model == 'customized':
-    tokenizer, llm = Customized(FLAGS.locally, FLAGS.ckpt)
   else:
     raise Exception('unknown model!')
-  
-  example_chain_ = example_chain(llm, tokenizer)
-  if FLAGS.mode == 'electrolyte':
-    chain = electrolyte_chain(llm, tokenizer)
-  elif FLAGS.mode == 'precursors':
-    chain = precursor_chain(llm, tokenizer)
-  elif FLAGS.mode == 'conductivity':
-    chain = conductivity_chain(llm, tokenizer)
-  else:
-    raise Exception('unknow mode!')
+
+  prompt, _ = extract_triplets_template(tokenizer)
+  graph_transformer = LLMGraphTransformer(
+    llm = llm, prompt = prompt,
+    allowed_nodes = ['electrolyte', 'conductivity', 'precursor', 'crystal_system_of_electrolyte', 'space_group_of_electrolyte'],
+    allowed_relationships = ['has_conductivity', 'has_precursor', 'has_crystal_system', 'has_space_group']
+  )
+  neo4j = Neo4jGraph(url = FLAGS.host, username = FLAGS.user, password = FLAGS.password, database = FLAGS.database)
 
   for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
     for f in files:
@@ -57,12 +51,9 @@ def main(unused_argv):
         loader = UnstructuredPDFLoader(join(root, f), mode = 'single', strategy = "hi_res")
       else:
         raise Exception('unknown format!')
-      text = ''.join([doc.page_content for doc in loader.load()])
-      example = example_chain_.invoke({'patent': text})
-      output = chain.invoke({'context': example})
-      results = {'example': example, 'output': output}
-      with open(join(FLAGS.output_dir, '%s_meta.txt' % splitext(f)[0]), 'w') as fp:
-        fp.write(json.dumps(results))
+      docs = loader.load()
+      graph = graph_transformer.convert_to_graph_documents(docs)
+      neo4j.add_graph_documents(graph)
 
 if __name__ == "__main__":
   add_options()

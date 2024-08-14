373165c0b99bdc6c6a330625a53da27e5ac8e12e
breadbread1984 <breadbread1984@163.com>
2024-08-14 02:13:08 +0000

    update

--- models.py
+++ models.py
@@ -99,3 +99,30 @@ def Qwen2(locally = False):
     )
   return tokenizer, llm
 
+def Qwen2_FA2(locally = False):
+  assert locally = True, "must be locally!"
+  class Qwen2FA2(LLM):
+    tokenizer: AutoTokenizer = None
+    model: AutoModelForCausalLM = None
+    def __init__(self,):
+      super().__init__()
+      self.tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2-7B-Instruct', trust_remote_code = True)
+      self.model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2-7B-Instruct', trust_remote_code = True)
+      self.model = self.model.to(device('cuda'))
+      self.model.eval()
+    def _call(self, prompt, stop = None, run_manager = None, **kwargs):
+      logits_processor = LogitsProcessorList()
+      logits_processor.append(TemperatureLogitsWarper(0.8))
+      logits_processor.append(TopPLogitsWarper(0.8))
+      inputs = self.tokenizer(prompt, return_tensors = 'pt')
+      inputs = inputs.to(device('cuda'))
+      outputs = self.model.generate(**inputs, logits_processor = logits_processor, use_cache = True, do_sample = True, max_length = 131072)
+      outputs = outputs.tolist()[0][len(inputs["input_ids"][0]):-1]
+      response = self.tokenizer.decode(outputs)
+      return response
+    @property
+    def _llm_type(self):
+      return "qwen2 with flast attention 2"
+  llm = Qwen2_FA2()
+  return llm.tokenizer, llm
+

eb1c02d566953c001ed8e4c46895b91f50b5ee74
breadbread1984 <breadbread1984@163.com>
2024-08-14 06:03:24 +0000

    update

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from os import environ
+import torch
 from torch import device
 from huggingface_hub import login
 from transformers import AutoTokenizer, AutoModelForCausalLM, LogitsProcessorList, \
